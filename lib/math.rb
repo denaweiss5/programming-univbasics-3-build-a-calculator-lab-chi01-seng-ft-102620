9f7cd7142da5f208a91b4d5c03a2b67320e23795
Dena Neuman <denaweiss5@gmail.com>
2020-09-30 03:10:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  subtraction = num1-num2
+  return subtraction
 end
 
 def division(num1, num2)
-
+  division= num2/num1
+  return division
 end
 
 def multiplication(num1, num2)
-
+  multiplication = num1*num2
+  return multiplication
 end
 
 def modulo(num1, num2)
-
+  modulo=num1%num2
+  return modulo
 end
 
 def square_root(num)
-
+  Math.square_root(num)
+  return Math.square_root(num)
 end
